78a6755d6c59ada4b81a13ef5f33561e27516343
Ankur Sharma <ankurs4@cisco.com>
2023-04-15 20:19:58 +0200

    chapter 2 finished

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
             pkgs.cargo
             pkgs.rustc
             pkgs.rustfmt
+            pkgs.rust-analyzer
           ];
         }
       );
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world, guess the number!");
+
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is {}", secret_num);
+
+    println!("Type your guess");
+    loop {
+        let mut guess: String = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read from stdin");
+
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Your guess is too low"),
+            Ordering::Greater => println!("Your guess is too high"),
+            Ordering::Equal => {
+                println!("Your guess is correct! You win!");
+                break;
+            },
+        }
+    }
 }
