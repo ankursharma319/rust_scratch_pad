095125669c565aea5fe733bf854780c9bf8a5ea1
Ankur Sharma <ankurs4@cisco.com>
2023-04-15 12:03:57 +0200

    nix: add flake instead of shell

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Rust
 
+## Usage
+
+```bash
+nix develop
+cargo build
+cargo run
+```
+
 `rustc` is the compiler. `cargo` is the build system and package manager. `cargo run` does the compilation and runs the binary.
+
+## Notes
+
+
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Rust scratchpad flake";
+  nixConfig.bash-prompt-suffix = "\[nix\] ";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      # Utilized by `nix develop`
+      devShell = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        pkgs.mkShell {
+          buildInputs = [
+            pkgs.cargo
+            pkgs.rustc
+            pkgs.rustfmt
+          ];
+        }
+      );
+      # utilized by `nix build .`
+      defaultPackage = forAllSystems(system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        pkgs.hello
+      );
+    };
+}
