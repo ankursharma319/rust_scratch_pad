63a5417c5f3e02855c9d1d90752412cf7fd17fb3
Ankur Sharma <ankurs4@cisco.com>
2023-11-23 13:26:36 +0100

    tokio_demo: use Arc Mutex for shared state

--- tokio_demo/src/main.rs
+++ tokio_demo/src/main.rs
@@ -8,39 +8,45 @@ use tokio::net::{TcpListener, TcpStream};
 // cargo run
 // cargo run --example hello-redis
 
+type DB = std::sync::Arc<std::sync::Mutex<std::collections::HashMap<String, bytes::Bytes>>>;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    println!("Hello, listening!");
     // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    let db:DB = std::sync::Arc::new(std::sync::Mutex::new(std::collections::HashMap::new()));
 
     loop {
         // The second item contains the IP and port of the new connection.
         let (socket, addr) = listener.accept().await.unwrap();
-        println!("new client: {:?}", addr);
+        println!("new client accepted: {:?}", addr);
+        let db = db.clone();
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: DB) {
     let mut redis_connection = mini_redis::Connection::new(socket);
-    let mut db: std::collections::HashMap<String, Vec<u8>>= std::collections::HashMap::new();
 
     while let Some(frame) = redis_connection.read_frame().await.unwrap() {
         println!("Got input frame = {:?}", frame);
         let response_frame = match mini_redis::Command::from_frame(frame).unwrap() {
             mini_redis::Command::Get(cmd) => {
                 println!("Got get command");
+                let db = db.lock().unwrap();
                 match db.get(cmd.key()) {
-                    Some(val) => { mini_redis::Frame::Bulk(val.clone().into()) }
+                    Some(val) => { mini_redis::Frame::Bulk(val.clone()) }
                     _ => { mini_redis::Frame::Null }
                 }
             },
             mini_redis::Command::Set(cmd) => {
                 println!("Got set command");
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 mini_redis::Frame::Simple("OK".to_string())
             },
             _ => panic!("Got another command"),
